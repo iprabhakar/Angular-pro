dcf04408e49aa5d336e5375fc7585043132772fa
iprabhakar <prabhakar.singh@weareflamingo.in>
2018-06-06 16:31:07 +0530

    update dev

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 //prabhakar singh hello;
-
+// new changes done here!!!!Done!
 module.exports = function (config) {
   config.set({
     basePath: '',
